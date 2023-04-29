9dbb88e2a39b1b73dedfe4e58615025cc171693d
yongyongkr <hahahaa_a@naver.com>
2023-04-29 17:20:00 +0900

    code style 적용

--- src/test/java/hello/advanced/trace/threadlocal/code/FieldService.java
+++ src/test/java/hello/advanced/trace/threadlocal/code/FieldService.java
@@ -5,21 +5,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class FieldService {
 
-    private String nameStore;
+  private String nameStore;
 
-    public String logic(String name) {
-        log.info("저장 name={} -> nameStore={}", name, nameStore);
-        nameStore = name;
-        sleep(1000);
-        log.info("조회 nameStore={}", nameStore);
-        return nameStore;
-    }
+  public String logic(String name) {
+    log.info("저장 name={} -> nameStore={}", name, nameStore);
+    nameStore = name;
+    sleep(1000);
+    log.info("조회 nameStore={}", nameStore);
+    return nameStore;
+  }
 
-    private void sleep(int millis) {
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+  private void sleep(int millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
 }
