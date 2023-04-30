48bdbeb29c457c127f4241ffdf655e5b597280ea
권용환 <hahahaa_a@naver.com>
2023-04-30 23:02:57 +0900

    익명 내부 클래스 사용

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
@@ -45,6 +46,27 @@ public class ContextV1Test {
     context1.execute();
 
     StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+    ContextV1 context2 = new ContextV1(strategyLogic2);
+    context2.execute();
+  }
+
+  @Test
+  void strategyV2() {
+    Strategy strategyLogic1 = new Strategy() {
+      @Override
+      public void call() {
+        log.info("비지니스 로직1 실행");
+      }
+    };
+    ContextV1 context1 = new ContextV1(strategyLogic1);
+    context1.execute();
+
+    Strategy strategyLogic2 = new Strategy() {
+      @Override
+      public void call() {
+        log.info("비지니스 로직2 실행");
+      }
+    };
     ContextV1 context2 = new ContextV1(strategyLogic2);
     context2.execute();
   }
